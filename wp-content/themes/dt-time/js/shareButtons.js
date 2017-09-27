6e29cf561ff99d9ef454a37aaca215a3f972766c
ByDraw <heavenlywing@foxmail.com>
2017-09-27 17:55:57 +0800

    change Douban sharing into Facebook sharing.

--- wp-content/themes/dt-time/js/shareButtons.js
+++ wp-content/themes/dt-time/js/shareButtons.js
@@ -1,5 +1,5 @@
 var shareNode =
-  '<div class="jiathis_style_24x24">	<a class="jiathis_button_tsina"></a>	<a class="jiathis_button_weixin"></a>	<a class="jiathis_button_cqq"></a>	<a class="jiathis_button_douban"></a></div>';
+  '<div class="jiathis_style_24x24">	<a class="jiathis_button_tsina"></a>	<a class="jiathis_button_weixin"></a>	<a class="jiathis_button_cqq"></a>	<a class="jiathis_button_fb"></a></div>';
 if (document.querySelector("#fancy-header .wf-table")) {
   document.querySelector("#fancy-header .wf-table").innerHTML += shareNode;
   with(document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src =
